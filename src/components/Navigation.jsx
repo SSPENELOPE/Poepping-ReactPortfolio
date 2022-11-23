b388f8ac522cf2b508fd98593b73548f63a8b162
SSPENELOPE <typoepping@gmail.com>
2022-11-23 17:09:58 -0500

    fixed navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
-import { React, useState } from react;
+import { React, useState } from "react";
+import Header from "./Header";
 import AboutMe from "./AboutMe";
 import MyProjects from "./MyProjects";
 import ContactMe from "./ContactMe";
@@ -7,16 +8,17 @@ function Navigation() {
     const [body, setBody] = useState(<AboutMe />)
     return (
         <div>
-            <div>
-                <Header body={body}/>
-            </div>
-              <h1>Tyler Poepping</h1>
+            <header>
+                <h1>Tyler Poepping</h1>
                 <nav>
                     <button className="navlink" onClick={() => setBody(<AboutMe />)}>About Me</button>
                     <button className="navlink" onClick={() => setBody(<MyProjects />)}>My Projects</button>
                     <button className="navlink" onClick={() => setBody(<ContactMe />)}>Contact Me</button>
                     <button className="navlink">Resume</button>
                 </nav>
+            </header>
+            <Header body={body} />
+
         </div>
     )
 }
